69b6b6846190f7b787e08979f1ed20c0c18323bd
leehis <ehinomence@gmail.com>
2023-01-23 11:53:47 -0500

    Initial Setup

---
+++ src/main/java/dev/leehis/leeflix/LeeflixApplication.java
@@ -0,0 +1,13 @@
+package dev.leehis.leeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/leehis/leeflix/LeeflixApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leehis.leeflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeeflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
